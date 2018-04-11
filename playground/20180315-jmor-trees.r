9cd37e48f4f7b7fdc249517a227e8a5ddc40a74e
George G. Vega Yon <g.vegayon@gmail.com>
2018-04-11 12:23:50 -0700

    Adding brute force posterior func

---
+++ playground/20180315-jmor-trees.r
@@ -0,0 +1,112 @@
+library(ape)
+library(aphylo)
+
+# Creating the tree ------------------------------------------------------------
+# tree <- matrix(c(3, 1, 3, 2), ncol=2,byrow=TRUE)
+# tree <- as.phylo(tree)
+# X <- c(1, 0)
+tree <- matrix(c(5, 1, 5, 2, 1, 3, 1, 4), ncol=2, byrow = TRUE)
+# tree <- matrix(c(5, 6, 5, 1, 6, 2, 6, 3, 6, 4), ncol=2, byrow = TRUE)
+tree <- as.phylo(tree)
+X    <- c(0, 0, 0)
+atree <- new_aphylo(X, tree)
+
+# Model parameters
+Pi  <- c(.7, .3)
+psi <- c(.01, .02)
+mu  <- c(.2, .1)
+
+tree_prob <- function(tree, psi, mu, Pi) {
+  # Generating a states matrix
+  states <- do.call(expand.grid, rep(list(c(0,1)), ape::Nnode(tree) + ape::Ntip(tree)))
+  colnames(states) <- paste0("node", 1:(ncol(states)))
+  
+  # Computing matrix of probabilities --------------------------------------------
+  # 2^(ntips + nnodes) 
+  PSI <- aphylo:::prob_mat(psi)
+  MU  <- aphylo:::prob_mat(mu)
+  
+  # For
+  Pr <- Pi[states[, ape::Ntip(tree) + 1] + 1]
+  for (i in 1:nrow(tree$edge)) {
+    e <- tree$edge[i,]
+    Pr <- Pr *
+      MU[cbind(states[, e[1]], states[, e[2]]) + 1]
+  }
+  
+  # Computing for each possible annotation of the tips
+  # 2^Ntip
+  Pr <- matrix(rep(Pr, 3), nrow=length(Pr), ncol = 2^ape::Ntip(tree))
+  states_tip <- do.call(expand.grid, rep(list(c(0,1)), ape::Ntip(tree)))
+  
+  for (i in 1:nrow(states_tip)) {
+    
+    for (j in 1:ape::Ntip(tree)) {
+      Pr[, i] <- Pr[, i] *
+        PSI[cbind(states[, j] + 1, states_tip[i, j] + 1)]
+    }
+    
+  }
+  
+  # Results
+  list(
+    Pr  = Pr,
+    row = states,
+    col = states_tip
+  )
+  
+}
+
+ans <- tree_prob(tree, psi, mu, Pi)
+
+sum(ans$Pr) # This should add up to 1
+
+# Comparing with aphylo --------------------------------------------------------
+l <- LogLike(atree, psi, mu, Pi[2])
+X <- as.vector(atree$tip.annotation)
+colSums(ans$Pr)[which(apply(ans$col, 1, function(x) all(x == X)))] -
+  exp(l$ll)
+
+exp(l$ll) # Jmorr tree 0.286622598, c(0, 0, 0)
+
+# Conditional probability ------------------------------------------------------
+
+i <- 4
+
+# Matching state
+state_col <- which(apply(ans$col, 1, function(x) all(x == X)))
+state_rows <- which(ans$row[,i] == 1)
+
+# Pr(root = 0 | data)
+sprintf(
+  "%.10f",
+  1 - sum(ans$Pr[state_rows, state_col])/sum(ans$Pr[, state_col]) # 0.9907319671
+)
+
+sprintf(
+  "%.10f",
+  l$Pr[4,1]*Pi[1]/
+    (l$Pr[4,2]*Pi[2] + l$Pr[4,1]*Pi[1])
+)
+
+i_isone <- which(ans$row[,i] == 1L)
+i_iszero <- which(ans$row[,i] == 0L)
+X_given_xi1 <- sum(ans$Pr[i_isone, state_col])/sum(ans$Pr[i_isone,])
+X_given_xi0 <- sum(ans$Pr[i_iszero, state_col])/sum(ans$Pr[i_iszero,])
+xi1 <- sum(ans$Pr[i_isone,])
+
+sprintf(
+  "%.10f",
+  1 - X_given_xi1/(X_given_xi1 + X_given_xi0*((1-xi1)/xi1))
+)
+
+
+
+
+# Jmorr tree
+# 
+# Pr(z1=0|X)	0.9907319671
+# Pr(z2=0|X)	0.9935955962
+# Pr(z3=0|X)	0.9931689347
+# Pr(z4=0|X)	0.9939582682
+# Pr(z5=0|X)	0.9939582682
