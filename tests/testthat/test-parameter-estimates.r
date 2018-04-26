94bb60fff5ec1c914e0bab64245f877613c4dcfa
George G. Vega Yon <g.vegayon@gmail.com>
2018-04-26 10:57:16 -0700

    Debugging MCMC [skip ci]

--- tests/testthat/test-parameter-estimates.r
+++ tests/testthat/test-parameter-estimates.r
@@ -55,15 +55,20 @@ test_that("MCMC: in a degenerate case all parameters goes to the prior", {
   dat <- suppressWarnings(sim_annotated_tree(10, Pi=0, mu=c(0, 0), psi=c(0,0)))
   dat$tip.annotation[] <- 9L
   
+  set.seed(1)
   ans1 <- suppressWarnings(
     aphylo_mcmc(rep(2/12, 7), dat, priors = function(x) dbeta(x, 2, 10),
-                control = list(nbatch = 2e4, useCpp=TRUE), check.informative = FALSE)
+                control = list(nbatch = 10, burnin=0L, useCpp=FALSE), check.informative = FALSE)
     )
   
+  set.seed(1)
   ans2 <- suppressWarnings(
-    aphylo_mcmc(rep(2/32, 7), dat, priors = function(x) dbeta(x, 2, 30),
-                control = list(nbatch = 2e4), check.informative = FALSE)
+    aphylo_mcmc(rep(2/12, 7), dat, priors = function(x) dbeta(x, 2, 10),
+                control = list(nbatch = 10, burnin=0L, useCpp=TRUE), check.informative = FALSE)
   )
+  
+  ans1
+  ans2
   
   # Should converge to the prior
   expect_equal(unname(coef(ans1)), rep(2/12, 7), tol=.05)
