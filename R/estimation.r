55006ea7dbc17319c53c8b5f8d77cf1007a1820e
George G. Vega Yon <g.vegayon@gmail.com>
2019-01-23 21:45:14 -0800

    Fixing bug in environments in the aphylo_mcmc and aphylo_mle

--- R/estimation.r
+++ R/estimation.r
@@ -181,7 +181,7 @@ aphylo_mle <- function(
   cl <- match.call()
   
   # Parsing the formula
-  env   <- parent.frame()
+  env   <- environment(model)
   model <- aphylo_formula(model, params, priors, env = env)
   
   if (any(model$fixed))
@@ -380,7 +380,7 @@ aphylo_mcmc <- function(
   cl <- match.call()
   
   # Parsing the formula
-  env   <- parent.frame()
+  env   <- environment(model)
   model <- aphylo_formula(model, params, priors, env = env)
 
   # Reducing the peeling sequence
